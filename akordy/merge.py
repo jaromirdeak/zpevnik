fe217d1747e5f99eabfad1265b1751db4cb4a585
Jan Remes <ciakval@gmail.com>
2020-06-07 11:16:47 +0200

    merge.py: refactor merging to a function

--- akordy/merge.py
+++ akordy/merge.py
@@ -3,17 +3,38 @@ locale.setlocale(locale.LC_ALL, '')
 
 init = os.path.join(os.path.dirname(sys.argv[0]), 'init.tex')
 
+
+def do_merge(songlist, outfile, initfile=init):
+    '''
+    Merge song files specified in 'songlist' to the output file 'outfile'.
+    Unless 'initfile' is None, its contents are prepended.
+    '''
+    with open(outfile, 'w', encoding='utf8') as fout:
+        if initfile is not None:
+            fout.write(open(initfile, 'r', encoding='utf8').read())
+
+        i = 1
+        for filename in songlist:
+            if not filename.endswith('.tex'):
+                continue
+            with open(filename, 'r', encoding='utf8') as fin:
+                first, rest = fin.read().strip().split('\n', 1)
+                fout.write(first)
+                fout.write('\\hypertarget{song-%i}{}\\label{song-%i}\n' % (i, i))
+                fout.write(rest)
+                fout.write('\n\n')
+            i += 1
+        fout.write('\n\\end{songs}\n\\end{document}\n')
+
+
+
 if __name__ == '__main__':
-  with open(sys.argv[2], 'w', encoding='utf8') as fout:
-    fout.write(open(init, encoding='utf8').read())
-    i = 1
-    for fn in sorted(os.listdir(sys.argv[1]), key=locale.strxfrm):
-      if '.tex' not in fn: continue
-      with open(os.path.join(sys.argv[1], fn), encoding='utf8') as fin:
-        first, rest = fin.read().strip().split('\n', 1)
-        fout.write(first)
-        fout.write('\\hypertarget{song-%i}{}\\label{song-%i}\n' % (i, i))
-        fout.write(rest)
-        fout.write('\n\n')
-      i += 1
-    fout.write('\n\\end{songs}\n\\end{document}\n')
+    if len(sys.argv) != 3:
+        print("ERROR: 2 arguments are required", file=sys.stderr)
+        sys.exit(1)
+
+    songdir=sys.argv[1]
+    outfile=sys.argv[2]
+    songlist = [ os.path.join(songdir, x) for x in sorted(os.listdir(songdir),key=locale.strxfrm) ]
+
+    do_merge(songlist, outfile)
